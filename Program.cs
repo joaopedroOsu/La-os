d57b1514df526689c336bd98c71f1f3c007eec5a
Joao <Joaopedroazevedo1980@gmail.com>
2026-05-22 20:43:30 -0300

    Laços

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Quantos devo exibir");
+int maximo = Convert.ToInt32(Console.ReadLine());
+int Contador = 0;
+
+while(Contador < maximo)
+{
+    if(Contador % 2 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+    }
+    Console.Write("Hello, World! ");
+int repeticoes = 0;
+while (repeticoes < 3)
+    {
+        
+    repeticoes = repeticoes + 1;
+    }
+Contador = Contador + 1;
+
+Console.WriteLine(Contador );
+}
+Console.ReadLine();
